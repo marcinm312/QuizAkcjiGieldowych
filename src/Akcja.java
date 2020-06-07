744f7377c444f3a5961797c65f5b1de6a681e9e1
Marcin Michalczyk <marcinm312@outlook.com>
2020-06-07 20:39:11 +0200

    Refactoring

--- src/Akcja.java
+++ src/Akcja.java
@@ -1,32 +1,25 @@
 public class Akcja {
 
-	private String nazwaAkcji;
-	private String skrotAkcji;
-	private String waluta;
-	private double kursAkcji;
+    private final String nazwaAkcji;
+    private final String skrotAkcji;
+    private final double kursAkcji;
 
-	public Akcja(String na, String sa, String wa, double ka) {
-		nazwaAkcji = na;
-		skrotAkcji = sa;
-		waluta = wa;
-		kursAkcji = ka;
+    public Akcja(String na, String sa, double ka) {
+        nazwaAkcji = na;
+        skrotAkcji = sa;
+        kursAkcji = ka;
+    }
 
-	}
+    public String getNazwaAkcji() {
+        return nazwaAkcji;
+    }
 
-	public String getNazwaAkcji() {
-		return nazwaAkcji;
-	}
+    public String getSkrotAkcji() {
+        return skrotAkcji;
+    }
 
-	public String getSkrotAkcji() {
-		return skrotAkcji;
-	}
-
-	public String getWaluta() {
-		return waluta;
-	}
-
-	public double getKursAkcji() {
-		return kursAkcji;
-	}
+    public double getKursAkcji() {
+        return kursAkcji;
+    }
 
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -8,68 +8,66 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Quiz {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		while (true) {
+        ArrayList<String> daneAkcji = new ArrayList<>();
+        odczytDanych(daneAkcji);
 
-			ArrayList<String> daneAkcji = new ArrayList<String>();
-			odczytDanych(daneAkcji);
+        String tempLine;
+        String[] tempWektorStr;
 
-			String tempLine = null;
-			String[] tempWektorStr = null;
+        ArrayList<Akcja> listaAkcji = new ArrayList<>();
+        for (int i = 0; i < daneAkcji.size(); i++) {
+            tempLine = daneAkcji.get(i);
+            tempWektorStr = tempLine.split(":");
+            Akcja tempAkcja = new Akcja(tempWektorStr[0], tempWektorStr[1],
+                    Double.parseDouble(tempWektorStr[3]));
 
-			ArrayList<Akcja> listaAkcji = new ArrayList<Akcja>();
-			for (int i = 0; i < daneAkcji.size(); i++) {
-				tempLine = daneAkcji.get(i);
-				tempWektorStr = tempLine.split(":");
-				Akcja tempAkcja = new Akcja(tempWektorStr[0], tempWektorStr[1], tempWektorStr[2],
-						Double.parseDouble(tempWektorStr[3]));
+            listaAkcji.add(i, tempAkcja);
+        }
 
-				listaAkcji.add(i, tempAkcja);
-			}
+        int punktacja;
+        int sumaPunktow = 0;
+        for (Akcja akcja : listaAkcji) {
+            System.out.println("\nPodaj giełdowy skrót spółki " + akcja.getNazwaAkcji()
+                    + " (ostatni kurs " + akcja.getKursAkcji() + "):");
+            Scanner scan = new Scanner(System.in);
+            String wartoscUsera = scan.nextLine().toUpperCase();
 
-			int punktacja = 0;
-			int sumaPunktow = 0;
-			for (int x = 0; x < listaAkcji.size(); x++) {
-				System.out.println("\nPodaj giełdowy skrót spółki " + listaAkcji.get(x).getNazwaAkcji()
-						+ " (ostatni kurs " + listaAkcji.get(x).getKursAkcji() + "):");
-				Scanner scan = new Scanner(System.in);
-				String wartoscUsera = scan.nextLine().toUpperCase();
+            if (wartoscUsera.equals(akcja.getSkrotAkcji())) {
+                punktacja = 3;
+                System.out.println("Poprawna odpowiedź, jesteś mistrzem i dostajesz 3 punkty :)");
+            } else {
+                punktacja = -2;
+                System.out.println("Niestety popełniłeś błąd. Przykro mi, ale muszę ci zabrać 2 punkty :(");
+            }
+            sumaPunktow = sumaPunktow + punktacja;
+            System.out.println("Aktualna punktacja: " + sumaPunktow + " pkt.");
+        }
+        System.out.println("\nKONIEC. Zdobyto " + sumaPunktow + " pkt.");
 
-				if (wartoscUsera.equals(listaAkcji.get(x).getSkrotAkcji())) {
-					punktacja = 3;
-					System.out.println("Poprawna odpowiedź, jesteś mistrzem i dostajesz 3 punkty :)");
-				} else {
-					punktacja = -2;
-					System.out.println("Niestety popełniłeś błąd. Przykro mi, ale muszę ci zabrać 2 punkty :(");
-				}
-				sumaPunktow = sumaPunktow + punktacja;
-				System.out.println("Aktualna punktacja: " + sumaPunktow + " pkt.");
-			}
-			System.out.println("\nKONIEC. Zdobyto " + sumaPunktow + " pkt.");
-		}
-	}
+    }
 
-	public static void odczytDanych(ArrayList<String> lista) {
-		Path currentRelativePath = Paths.get("");
-		String s = currentRelativePath.toAbsolutePath().toString();
-		System.out.println("Current relative path is: " + s);
+    public static void odczytDanych(ArrayList<String> lista) {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current relative path is: " + s);
 
-		try {
-			FileReader fr = new FileReader(/*"AkcjeQuiz/" +*/ "resources/akcje.txt");
-			BufferedReader bfr = new BufferedReader(fr);
-			String odczytanaLinia = null;
-			int idxListyDanych = 0;
-			while ((odczytanaLinia = bfr.readLine()) != null) {
-				lista.add(idxListyDanych, odczytanaLinia);
-				idxListyDanych++;
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Nie znaleziono pliku");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Pojawił sie błąd wejscia wyjścia");
-			e.printStackTrace();
-		}
-	}
+        try {
+            FileReader fr = new FileReader(/*"AkcjeQuiz/" +*/ "resources/akcje.txt");
+            BufferedReader bfr = new BufferedReader(fr);
+            String odczytanaLinia;
+            int idxListyDanych = 0;
+            while ((odczytanaLinia = bfr.readLine()) != null) {
+                lista.add(idxListyDanych, odczytanaLinia);
+                idxListyDanych++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie znaleziono pliku");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Pojawił sie błąd wejścia wyjścia");
+            e.printStackTrace();
+        }
+    }
 }
